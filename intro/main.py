89eb961f471deb88a8db6cd8fc6caf5dda244483
KengoWada <louiskengo3@gmail.com>
2019-09-19 15:59:28 +0300

    Added sample code to get started with NetworkX

---
+++ intro/main.py
@@ -0,0 +1,18 @@
+import networkx as nx
+
+# Create an "empty" graph
+G = nx.Graph()
+
+# Add nodes to the graph
+G.add_node("A")
+G.add_node("B")
+G.add_node("C")
+# TODO: Add nodes D and E
+# Code goes here
+
+
+# Add edges to the graph
+G.add_edge("A", "C")
+G.add_edge("B", "C")
+# TODO: Add edges CD, DA and BE
+# Code goes here
